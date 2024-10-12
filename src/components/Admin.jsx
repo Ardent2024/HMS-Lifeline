492724b8d3201091ab703868324ed595870d372d
Ardent2024 <ardentproject602@gmail.com>
2024-10-12 14:58:53 +0530

    Update Admin.jsx

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -7,8 +7,8 @@ const Admin = () => {
   const [appointments, setAppointments] = useState([]);
 
   useEffect(() => {
-    // Fetch appointments data from backend
-    axios.get('http://localhost:5050/getAppointments')
+    // Fetch appointments data from the new backend URL
+    axios.get('https://admin-pannel-77fy.onrender.com/getAppointments')
       .then(response => setAppointments(response.data))
       .catch(err => console.log(err));
   }, []);
@@ -21,55 +21,52 @@ const Admin = () => {
 
   return (
     <>
-
       {/* Navbar */}
-
       <header className="logo2 life">
-                  <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top hii">
-                    <div className="container-fluid ">
-                      <div className="icon2"></div>
-                      <div className="life1">
-                        <i className="sahil">LifeLine</i>
-                      </div>
-                      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                      </button>
-                      <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul className="navbar-nav">
-                          <li className="nav-item">
-                            <Link to="/" className="nav-link active mt-3 ho" aria-current="page" >Home</Link>
-                          </li>
-                          <li className="nav-item">
-                            <Link to="/Doctor" className="nav-link active mt-3 ho">Doctor with us</Link>
-                          </li>
-                          <li className="nav-item">
-                            <Link to="/Health" className="nav-link active mt-3 ho">Health package</Link>
-                          </li>
-                          <li className="nav-item">
-                            <Link to="/Emergency" className="nav-link active mt-3 ho">Emergency</Link>
-                          </li>
-                          <li className="nav-item">
-                            <Link to="/Online_r" className="nav-link active mt-3 ho" >Online report</Link>
-                          </li>
-                          <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle active mt-3" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                              More
-                            </a>
-                            <ul className="dropdown-menu">
-                              <li><Link to="/Feedback" className="dropdown-item">Feedback</Link></li>
-                              <li><Link to="/Cont" className="dropdown-item">Contact Us</Link></li>
-                              <li><Link to="/Adminlogin" className="dropdown-item">Admin</Link></li>
-                            </ul>
-                          </li>
-                          <li className="nav-item">
-                            <Link to="/Logins"><button className="book">Book Appointment</button></Link>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </nav>
-                </header>
-
+        <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top hii">
+          <div className="container-fluid ">
+            <div className="icon2"></div>
+            <div className="life1">
+              <i className="sahil">LifeLine</i>
+            </div>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link to="/" className="nav-link active mt-3 ho" aria-current="page">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/Doctor" className="nav-link active mt-3 ho">Doctor with us</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/Health" className="nav-link active mt-3 ho">Health package</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/Emergency" className="nav-link active mt-3 ho">Emergency</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/Online_r" className="nav-link active mt-3 ho">Online report</Link>
+                </li>
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle active mt-3" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    More
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li><Link to="/Feedback" className="dropdown-item">Feedback</Link></li>
+                    <li><Link to="/Cont" className="dropdown-item">Contact Us</Link></li>
+                    <li><Link to="/Adminlogin" className="dropdown-item">Admin</Link></li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <Link to="/Logins"><button className="book">Book Appointment</button></Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
 
       {/* Sidebar for navigation */}
       <div className="sidebar">
@@ -128,9 +125,6 @@ const Admin = () => {
                 <td>{appointment.doctor}</td>
               </tr>
             ))}
-
-
-
           </tbody>
         </table>
       </div>
